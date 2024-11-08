199b51e3f6d2defe9a803159b94ae9430b646f75
javiluli <javiluli.dev@gmail.com>
2024-11-08 15:02:56 +0100

    se ha modificado la funciona para calcular la fecha y hora

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,6 +3,14 @@ export function time(eventos) {
   const opcionesFecha = { weekday: 'long', day: 'numeric', month: 'long' }
   const fechaFormateada = fecha.toLocaleDateString('es-ES', opcionesFecha)
 
+  // Convertir la hora a la zona horaria de Madrid
+  const opcionesHora = { hour: '2-digit', minute: '2-digit', hour12: false, timeZone: 'Europe/Madrid' }
+  const horaFormateada = fecha.toLocaleTimeString('es-ES', opcionesHora)
+
+  // Determinar si es horario de verano (CEST) o invierno (CET)
+  const isDaylightSaving = fecha.toLocaleTimeString('es-ES', { timeZoneName: 'short', timeZone: 'Europe/Madrid' }).includes('CEST')
+  const timezoneAbbr = isDaylightSaving ? 'CEST' : 'CET'
+
   const mes = fecha.getMonth() + 1
   const dia = fecha.getDate()
   const clave = `${mes}-${dia}`
@@ -11,8 +19,8 @@ export function time(eventos) {
 
   if (eventosEspeciales.length > 0) {
     const eventosTexto = eventosEspeciales.map((evento) => `${evento.evento} ${evento.emojis}`).join(', ')
-    return `${fechaFormateada.charAt(0).toUpperCase() + fechaFormateada.slice(1)}, ${fecha.getHours()}:${fecha.getMinutes()} CET - ${eventosTexto}`
+    return `${fechaFormateada.charAt(0).toUpperCase() + fechaFormateada.slice(1)}, ${horaFormateada} ${timezoneAbbr} - ${eventosTexto}`
   } else {
-    return `${fechaFormateada.charAt(0).toUpperCase() + fechaFormateada.slice(1)}, ${fecha.getHours()}:${fecha.getMinutes()} CET`
+    return `${fechaFormateada.charAt(0).toUpperCase() + fechaFormateada.slice(1)}, ${horaFormateada} ${timezoneAbbr}`
   }
 }
